0b877ef248caa290ef610f38dd5a35456c6d9901
Jackbui96 <nguyenbui1996@gmail.com>
2019-05-09 11:51:04 -0700

    Pascal's Triangle-Java

--- 118. Pascal's Triangle - Java/src/Main.java
+++ 118. Pascal's Triangle - Java/src/Main.java
@@ -4,28 +4,35 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println(generate(4));
     }
 
     private static List<List<Integer>> generate(int numRows) {
+
         List<List<Integer>> triangle = new ArrayList<>();
 
-        // First base case; if user requests zero rows, they get zero rows.
-        if (numRows == 0) {
+        if (numRows == 0)
             return triangle;
-        }
 
-        for (int line = 1; line <= numRows; line++) {
-            List<Integer> row = new ArrayList<>();
-            row.add(1);
-            for (int i = 1; i <= )
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+        triangle.add(row);
 
+        if (numRows == 1)
+            return triangle;
+
+        for (int line = 2; line <= numRows; line++) {
+            List<Integer> row2 = new ArrayList<>();
+            row2.add(1);
+            for (int i = 1; i < line; i++) {
+                int num = row2.get(i - 1) * (line - i) / i;
+                row2.add(num);
+            }
+            triangle.add(row2);
         }
 
         return triangle;
     }
-
 }
 
 
